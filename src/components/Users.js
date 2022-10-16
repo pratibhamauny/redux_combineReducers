c960c95f33bebba7978f14e05626f8f0c6116665
Pratibha Patel <dvpatel@pratibha-mm.local>
2022-10-16 02:17:55 -0700

    user component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import {connect} from 'react-redux'
 const Users = (props) => {
-    console.log(props.users)
-  
-   
+   // console.log(props.users.users.length)
+   const usersData=props.users.users
+   const usersList=usersData.map(user=>{
+    return <tr><td>{user.name}</td>
+    <td>{user.email}</td>
+    <td>{user.contact}</td></tr>
+   })
   return (
-    <div>
-    <h2>User List</h2>
-    <button onClick={props.showUsers}>Show Data</button>
-   
-        <table width="600px" cellSpacing="5px">
-            <thead>
-                <tr><th>Name</th>
-                <th>Email</th>
-                <th>Contact</th>
-                </tr>
-            </thead>
-        </table>
-   
-    </div>
+   <div>
+    {props.users.users.length>0?
+    <table width="600px" cellPadding="5px" cellSpacing="3px" border="1px" align="center">
+        <thead>
+            <tr><th>Name</th>
+            <th>Email</th>
+            <th>Contact No.</th></tr>
+            {usersList}
+        </thead>
+    </table>:<p>No user Data</p>}
+   </div>
+    
   )
 }
 
